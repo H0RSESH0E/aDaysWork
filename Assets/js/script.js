aa53b1aebf4c42a24ae1712ba93729883cf3e0f4
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-01-28 15:11:49 -0500

    working on automatic time update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,13 @@
 var today = moment().format('LL');
 var dayBegin = 9;
+var hoursInTheDay = 8;
 var classesInNOut;
 var lastContent;
 var activities = [];
+var initialLoad = true;
+var countDownToStyleChecks = (60 - moment().minutes());
+console.log(countDownToStyleChecks)
+
 
 var fillSlot = function(slotTime, slotText) {
     
@@ -11,13 +16,13 @@ var fillSlot = function(slotTime, slotText) {
     var slotLi = $("<li>")
         .addClass("row");
     var slot = $("<span>")
-        .addClass("col-1 hour")
+        .addClass("col-2 col-sm-1 hour")
         .text(slotTime);
     var activity = $("<p>")
-        .addClass("col-10 description" + " " + timeStyle)
+        .addClass("col-8 col-sm-10 description" + " " + timeStyle)
         .text(slotText);
     var btn = $("<button>")
-        .addClass("col-1 bi bi-save2 saveBtn")
+        .addClass("col-2 col-sm-1 bi bi-save2 saveBtn")
 
     
 
@@ -28,7 +33,7 @@ var fillSlot = function(slotTime, slotText) {
 }
 
 var styleOnTime = function (slotTime) {
-
+ console.log("running style check");
     var timeToCheck = moment(slotTime, "hA");
     
     if (moment().isSame(timeToCheck, "h")) {
@@ -46,26 +51,28 @@ var styleOnTime = function (slotTime) {
 var loadDailyActivityList = function() {
 
     activities = JSON.parse(localStorage.getItem("activities"));
-
+if (initialLoad) {
     if (!activities) {
         activities = [];
         var tempArr = [];
         var timeString;
         var timeSlot;
-        for (var i =0; i < 8; i++) {
+        for (var i =0; i < hoursInTheDay; i++) {
 
             timeString = " " + (i + dayBegin) +":00"
             console.log(timeString);
             timeSlot = moment(today + timeString).format("hA");
            
-            tempArr = [timeSlot,"Placeholders"];
+            tempArr = [timeSlot,""];
             activities.push(tempArr);
         }
+        localStorage.setItem("activities", JSON.stringify(activities));
     }
 
-    for (var i =0; i < 8; i++) {
+    for (var i =0; i < hoursInTheDay; i++) {
         fillSlot(activities[i][0], activities[i][1])
     }
+}
 
 }
 
@@ -92,7 +99,9 @@ $("ul").on("click", "p", function() {
   
     lastContent = text;
 
-    var textInput = $("<textarea>").addClass("textarea col-10").val(text);
+    var timeStyle = styleOnTime($(this).siblings(".hour").text());
+
+    var textInput = $("<textarea>").addClass("textarea col-10 " + timeStyle).val(text);
     $(this).replaceWith(textInput);
   
     textInput.trigger("focus");
@@ -138,9 +147,64 @@ $("ul").on("blur", "textarea", function() {
     
 });
 
+// $("ul").on("keydown", "textarea", function() {
+
+//     if ()
+//     var text = $(this).val();
+  
+//     if (text === lastContent) {
+        
+//         $(this).siblings(".saveBtn").removeClass("not-saved")
+//     }   // if didn't change take highlight off save button
+
+
+
+//     var index = $(this)
+//       .closest("li")
+//       .index();
+  
+//     activities[index][1] = text;
+  
+//     var activity = $("<p>")
+//         .addClass(classesInNOut)
+//         .text(text);
+        
+//     styleOnTime($(activity).closest(".hour"));
+//     $(this).replaceWith(activity);
+
+
+
+// });
+
+var userSubmitResponseHandler = function (event) {
+
+    if (event.keyCode === 13 ) {
+        lastPlayer = document.querySelector("input").value.toUpperCase();
+        console.log(lastPlayer);
+        var newRecordArray = [lastPlayer, userScore];
+    }
+}
+
+var hourlyStyleUpdate = function( ){
+    var currentHourStringToCheck = moment().format("hA");
+    var lastHourStringToCheck = moment().subtract(1, "hours").format("hA");
+
+    console.log(currentHourStringToCheck);
+    console.log(lastHourStringToCheck);
+
+    console.log($("ul, li, span:contains(" + currentHourStringToCheck + ")").siblings("p").addClass("present"));
+    console.log($("ul, li, span:contains(" + currentHourStringToCheck + ")").siblings("p").removeClass("future"));
+    console.log($("ul, li, span:contains(" + lastHourStringToCheck + ")").siblings("p").addClass("past"));
+    console.log($("ul, li, span:contains(" + lastHourStringToCheck + ")").siblings("p").removeClass("present"));
+}
+
 
 $("#currentDay").append(" " + today);
-
 loadDailyActivityList();
 
-console.log(activities);
+
+// addEventListener("keydown", userSubmitResponseHandler);
+console.log($("li"));
+// setCountdown(hourlyStyleUpdate, countDownToStyleChecks*60*1000)
+
+hourlyStyleUpdate();
